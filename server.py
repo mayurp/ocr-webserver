ea5bebb8ab4c5e8ab516e11b0bab55816f1fb864
Mayur Patel <mayurp@gmail.com>
2016-02-07 01:25:31 +0800

    Use tornado as webserver instead of flask

--- server.py
+++ server.py
@@ -5,33 +5,80 @@
 
 __author__ = "Mayur Patel"
 
+
 import argparse
-from flask import Flask, jsonify, request, Response
-import ocr
-#from ocr import DownloadError
 import logging
 from logging import Formatter, FileHandler
-import search
+import json
+import tornado
+import tornado.options
+import tornado.httpserver
+from tornado.ioloop import IOLoop
+from tornado.gen import coroutine
+from tornado import web
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+from tornado_smack import App
+import random, time
+import sys
 
-_VERSION = 1
+#import ocr
+#import search
 
-app = Flask(__name__)
+
+app = App()
 
 
-class APIError(Exception):
-    status_code = 400
+class APIError(web.HTTPError):
+    def __init__(self, message, status_code=400):
+        web.HTTPError.__init__(self, status_code, reason=message)
+        
+    # def to_dict(self):
+    #     rv = dict(self.payload or ())
+    #     rv['error'] = self.message
+    #     return rv
 
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
 
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['error'] = self.message
-        return rv
+# Simple Tornado base handler with error handling
+class BaseHandler(tornado.web.RequestHandler):
+    def __init__(self,application, request,**kwargs):
+        super(BaseHandler,self).__init__(application,request)
+
+    def write_error(self, status_code, **kwargs):
+        self.set_header('Content-Type', 'text/json')
+        self.finish(json.dumps({}))
+
+    #def _handle_request_exception(self, e):
+    #    logging.error('error')
+        #write_error(self, 500, e)
+
+class DefaultHandler(BaseHandler):
+    pass
+
+
+# class MyHandler(RequestHandler):
+#     def __init__(self, loop=None):
+#         print "making handler"
+#         self.executor = futures.ThreadPoolExecutor(4)
+#         self.loop = loop or IOLoop.instance()
+
+#     @run_on_executor
+#     def long_running_task(self):
+#         tau = random.randint(0, 3)
+#         print "about to sleep for ", tau
+#         time.sleep(tau)
+#         return tau
+
+
+#class MyHandler(tornado.web.RequestHandler):
+
+#@run_on_executor
+def long_running_task(i):
+    print "hreeer long_running_task"
+    #tau = random.randint(0, i)
+    time.sleep(i)
+    print "end long_running_task"
+    return i
 
 
 @app.route("/")
@@ -40,67 +87,85 @@ def hello():
 
 
 # Run OCR on provided image and return UT8 text. OCR meta data is persisted  
-@app.route('/v{}/ocr'.format(_VERSION), methods=["POST"])
-def ocr_api():
+@app.route('/ocr', methods=["POST"])
+def ocr_api(self):
     try:
-        url = request.json['image_url']
-        lang = request.json.get('lang', 'eng')
+        request_json = json.loads(self.request.body)
+        url = request_json['image_url']
+        lang = request_json.get('lang', 'eng')
     except:
         raise APIError("Did you mean to send: {'image_url': 'some_jpeg_url'}")
 
     try:
-        text, _ = ocr.process_image(url, lang, store_data=True)
-        response = jsonify({"output": text})
+        text, _ = "test", "" #ocr.process_image(url, lang, store_data=True)
         # workaround to set charset to utf8
-        response.headers['Content-Type'] = 'application/json; charset=utf-8'
-        return response
-    except ocr.DownloadError as e:
-        raise APIError(e.message)
+        self.set_header('Content-Type', 'application/json')
+        self.write(json.dumps({"output" : text}))
+    #except ocr.DownloadError as e:
+    #    raise APIError(e.message)
     except Exception as e:
         raise APIError("Failed to process image: %s" % e.message, status_code=500)
 
 
 # Search OCR meta data of processed images
-@app.route('/v{}/search'.format(_VERSION), methods=["GET", "POST"])
-def search_api():
+@app.route('/search', methods=["GET", "POST"], handler_bases=(BaseHandler,))
+def search_api(self):
     try:
-        keywords = request.json['keywords']
-        page = int(request.json['page'])
-        page_size = int(request.json['page_size'])
+        request_json = json.loads(self.request.body)
+        #print self.request.body
+        keywords = request_json['keywords']
+        page = int(request_json['page'])
+        page_size = int(request_json['page_size'])
     except:
         raise APIError("Did you mean to send: {'keywords': 'someword', 'page' : 1, 'page_size' : 10 }")
     try:
-        results_xml = search.search_results_as_xml(keywords, page, page_size)
-        return Response(results_xml, mimetype='text/xml')
+        results_xml = "<test>"#search.search_results_as_xml(keywords, page, page_size)
+        #return Response(results_xml, mimetype='text/xml')
+        self.set_header('Content-Type', 'text/xml')
+        self.write(results_xml)
     except Exception as e:
         raise APIError("Unexpected error during search: %s" % e.message, status_code=500)
 
+
+# def ocr_task(url):
+#     s = 0
+#     for i in range(0, 10000*1000):
+#         s += i
+#     return s
+
+# @app.route('/test', methods=["GET"], handler_bases=(BaseHandler,))
+# @coroutine
+# def test_api(self):
+#     blah = self.request.body
+#     #res = yield self.long_running_task(3)
+#     res = yield ocr_executor.submit(long_running_task, 3) 
+#     self.write("--test done : %d\n" % res)
+
+# @app.route('/quick', methods=["GET"], handler_bases=(BaseHandler,))
+# @coroutine
+# def quick_api(self):
+#     #blah = self.request.body
+#     #search_executor
+#     self.write("------ quick done\n") 
+
+
 # Error handlers
 
-@app.errorhandler(500)
-def internal_error(error):
-    logging.info(str(error))
+# @app.errorhandler(500)
+# def internal_error(error):
+#     logging.info(str(error))
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    logging.info(str(error))
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     logging.info(str(error))
 
 
-@app.errorhandler(APIError)
-def handle_api_error(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
-
-## TODO remove?
-# # Catch all unexpected exceptions
-# @app.errorhandler(Exception)
-# def handle_internal_error(error):
-#     response = jsonify("Internal Server Error: %s", error.message)
-#     response.status_code = 500 
+# @app.errorhandler(APIError)
+# def handle_api_error(error):
+#     response = error.to_dict()
+#     response.status_code = error.status_code
 #     return response
-
 
 def init_logging(debug=False):
     if not debug:
@@ -118,10 +183,29 @@ def init_logging(debug=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--debug', action='store_true', help="For development only. Allows auto reloading of code")
-    parser.add_argument('--host', default="127.0.0.1")
+    #parser.add_argument('--host', default="127.0.0.1")
     parser.add_argument('--port', type=int, default=5000)
     args = parser.parse_args()
+    sys.argv = ""
 
-    init_logging(args.debug)
+    tornado.options.parse_command_line()
+    logging.info('Starting up')
+    proc_count = multiprocessing.cpu_count()
+    logging.info("Number of cpu cores: %d", proc_count)
+    application = tornado.web.Application(app.get_routes(), default_handler_class=DefaultHandler, debug=args.debug)
+    server = tornado.httpserver.HTTPServer(application)
+    server.bind(args.port)
+    
+    if args.debug:
+        server.start(1)
+    else:
+        server.start(0)    
 
-    app.run(host=args.host, port=args.port, debug=args.debug)
+    num_ocr_processes = max(1, proc_count / 2)
+    #num_search_pocesses = proc_count
+    logging.info("OCR process pool size: %d", num_ocr_processes)
+    ocr_executor = ProcessPoolExecutor(num_ocr_processes)
+    #search_executor = ProcessPoolExecutor(num_search_pocesses)
+    #logging.info("Search process pool size: %d", num_search_pocesses)
+
+    IOLoop.current().start()
